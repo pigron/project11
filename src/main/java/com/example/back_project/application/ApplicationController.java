3e20a4289ad1483e81aba549e9517530071cd6bc
문재영 <jy.moon@okestro.com>
2023-09-19 14:15:41 +0900

    123

--- src/main/java/com/example/back_project/application/ApplicationController.java
+++ src/main/java/com/example/back_project/application/ApplicationController.java
@@ -9,11 +9,13 @@ public class ApplicationController {
 
     @PostMapping("/test/map")
     public Map<String, Object> test(@RequestBody Map<String, Object> body) {
+        System.out.println(body);
         return body;
     }
 
     @PostMapping("/test/string")
     public String test1(@RequestBody String body) {
+        System.out.println(body);
         return body;
     }
 
